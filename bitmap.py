32ffba3378ff44c22a6fd96e2129365b71a8de4c
Angelica Miranda <am@MacBook-Air-de-Angelica.local>
2020-12-02 21:43:42 -0300

    ex1

--- bitmap.py
+++ bitmap.py
@@ -6,33 +6,23 @@
 #    By: anolivei <anolivei@student.42sp.org>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/11/28 21:22:46 by anolivei          #+#    #+#              #
-#    Updated: 2020/12/02 21:35:55 by anolivei         ###   ########.fr        #
+#    Updated: 2020/12/02 21:42:49 by anolivei         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-from collections import Counter
 import numpy as np
 
 def bitmap(A, M):
     print 'The vector is:', A
     print 'The matrix is: ', M
-    ret =  dict(sum(map(Counter, M), Counter()))
-    print (ret)
-    res = [ret[i] for i in A if i in ret]
-    print (res)
-    print (A.shape)
-    c = []
-    print (np.sum(M == A[3]))
-    i = 0
-    while (i < 4):
-        c.append(np.sum(M == A[i]))
-        i+=1
-    c=[]
+    ret=[]
     for i in range(len(A)):
-        c.append(np.sum(M == A[i]))
-    print (c)
+        ret.append(np.sum(M == A[i]))
+    print "The frequency of vector's elements in the matrix is:", ret
 
 if __name__ == '__main__':
+    # Define your vector here
     A = np.array([0, 1, 2, 6])
+    # Define your matrix here
     M = np.array([[0, 0, 5 , 7, 8, 9],[1, 1, 8, 9, 2, 3]])
     bitmap(A, M)
